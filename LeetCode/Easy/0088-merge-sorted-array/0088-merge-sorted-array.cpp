c80f2f36280339353d0884344210c1981b70416c
Ibrahim-Elsayed-27 <95621621+Ibrahim-Elsayed-27@users.noreply.github.com>
2025-11-22 21:24:54 +0200

    0088-merge-sorted-array .cpp Easy 11-22-2025 0 ms (100%) 12.3 MB (38.99%)

--- LeetCode/Easy/0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ LeetCode/Easy/0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> copy_merge;
-        int nums1_iter = 0;
-        int nums2_iter = 0;
-        while(nums1_iter < m && nums2_iter < n){
-            if(nums1[nums1_iter] < nums2[nums2_iter]){
-                copy_merge.push_back(nums1[nums1_iter]);
-                ++nums1_iter;
+        int nums1_back_iter = m - 1;
+        int nums2_back_iter = n - 1;
+        int merged_back_iter = m + n - 1;
+        while(nums1_back_iter >= 0 && nums2_back_iter >= 0){
+            if(nums1[nums1_back_iter] > nums2[nums2_back_iter]){
+                nums1[merged_back_iter] = nums1[nums1_back_iter];
+                --nums1_back_iter;
+
+            }else{
+                nums1[merged_back_iter] = nums2[nums2_back_iter];
+                --nums2_back_iter;             
             }
-            else{
-                copy_merge.push_back(nums2[nums2_iter]);
-                ++nums2_iter;
+
+            --merged_back_iter;
+        }
+
+
+        if(nums2_back_iter >= 0){
+            while(nums2_back_iter >= 0){
+                nums1[merged_back_iter] = nums2[nums2_back_iter];
+                --nums2_back_iter;  
+                --merged_back_iter; 
             }
         }
-        if(nums1_iter < m){
-            while(nums1_iter < m){
-                copy_merge.push_back(nums1[nums1_iter]);
-                ++nums1_iter;
-            }
-        }
-        if(nums2_iter < n){
-            while(nums2_iter < n){
-                copy_merge.push_back(nums2[nums2_iter]);
-                ++nums2_iter;
-            }
-        }
-        
-        nums1.swap(copy_merge);
+
     }
 };
