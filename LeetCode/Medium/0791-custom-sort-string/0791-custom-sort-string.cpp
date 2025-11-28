10749097d4d698e83471a1c3a2b2d5c8829e0a01
Ibrahim-Elsayed-27 <95621621+Ibrahim-Elsayed-27@users.noreply.github.com>
2025-11-28 14:15:19 +0200

    0791-custom-sort-string .cpp Medium 11-28-2025 0 ms (100%) 8.4 MB (50.02%)

--- LeetCode/Medium/0791-custom-sort-string/0791-custom-sort-string.cpp
+++ LeetCode/Medium/0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     string customSortString(string order, string s) {
-        vector<int> order_freq_priority(26, -1);
-        int order_size =  order.size();
-        for(int i=0; i< order_size; ++i){
-            order_freq_priority[order[i] - 97] = order_size - i - 1;
-        }
-        priority_queue<pair<int, char>> s_max_heap;
-        for(char ch: s){
-            s_max_heap.push(make_pair(order_freq_priority[ch - 97], ch));
-        }
-        int i = 0;
-        while(!s_max_heap.empty()){
-            s[i] = s_max_heap.top().second;
-            s_max_heap.pop();
-            ++i;
+        vector<int> freq(26, 0);
+        for(char ch : s){
+            freq[ch - 'a']++;
         }
 
+        string result = "";
+        for(char ch : order){
+            while(freq[ch - 'a'] > 0){
+                result += ch;
+                freq[ch - 'a']--;
+            }
+        }
 
+        for(int i = 0; i < 26; ++i){
+            while(freq[i] > 0){
+                result += char(i + 'a');
+                freq[i]--;
+            }
+        }
 
-        return s;
+        return result;
     }
 };
