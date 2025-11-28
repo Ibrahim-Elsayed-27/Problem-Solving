3b966924af417dc21e5e99b9034d0a6db4368174
Ibrahim-Elsayed-27 <95621621+Ibrahim-Elsayed-27@users.noreply.github.com>
2025-11-28 14:52:20 +0200

    1795-rearrange-products-table .sql Easy 11-28-2025 502 ms (54.65%) 0B (100%)

---
+++ LeetCode/Easy/1795-rearrange-products-table/1795-rearrange-products-table.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+select product_id , 'store1' As store, store1 as price
+from products
+where store1 is not null
+
+union all
+
+select product_id , 'store2' As store, store2 as price
+from products
+where store2 is not null
+
+union all
+
+select product_id , 'store3' As store, store3 as price
+from products
+where store3 is not null
