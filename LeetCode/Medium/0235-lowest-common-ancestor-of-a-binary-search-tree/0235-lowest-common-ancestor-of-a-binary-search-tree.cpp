26239e9f50cddd2e99390720b823e2e976dffbbc
Ibrahim-Elsayed-27 <95621621+Ibrahim-Elsayed-27@users.noreply.github.com>
2025-12-03 00:28:06 +0200

    0235-lowest-common-ancestor-of-a-binary-search-tree .cpp Medium 12-03-2025 19 ms (83.35%) 23.4 MB (89.89%)

--- LeetCode/Medium/0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ LeetCode/Medium/0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -17,7 +17,8 @@ public:
         else{
             if(root-> val > p->val && root-> val > q->val){
                 return lowestCommonAncestor(root->left, p, q);
-            }else if(root-> val < p->val && root-> val < q->val){
+            }else if((root-> val < p->val && root-> val < q->val) || 
+                    (root-> val < q->val && root-> val < p->val)){
                 return lowestCommonAncestor(root->right, p, q);
             }else{
                 return root;
