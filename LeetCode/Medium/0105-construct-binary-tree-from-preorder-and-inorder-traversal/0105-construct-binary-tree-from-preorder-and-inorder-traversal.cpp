806b002ad639ec6129df28e353800eadcdc8c69d
Ibrahim-Elsayed-27 <95621621+Ibrahim-Elsayed-27@users.noreply.github.com>
2025-12-05 13:46:53 +0200

    0105-construct-binary-tree-from-preorder-and-inorder-traversal .cpp Medium 12-05-2025 7 ms (39.11%) 26.9 MB (96.73%)

--- LeetCode/Medium/0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ LeetCode/Medium/0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -20,7 +20,7 @@ public:
     TreeNode* build(vector<int>& pre, vector<int>& in,
                     int preL, int preR, int inL, int inR) {
 
-        if (preL > preR || inL > inR) 
+        if (preL > preR || inL > inR)
             return nullptr;
 
         int rootVal = pre[preL];
@@ -29,45 +29,24 @@ public:
         int k = inL;
         while (k <= inR && in[k] != rootVal) k++;
 
-        if (k > inL && preL + 1 <= preR) {
-            int leftVal = pre[preL + 1];
-            bool existsLeft = false;
-            for (int i = inL; i < k; i++) {
-                if (in[i] == leftVal) {
-                    existsLeft = true;
-                    break;
-                }
-            }
-            if (existsLeft) {
-                int leftSize = k - inL;
-                root->left = build(pre, in,
-                                   preL + 1, preL + leftSize,
-                                   inL, k - 1);
-            }
+        int leftSize = k - inL;
+
+        if (leftSize > 0) {
+            root->left = build(pre, in,
+                               preL + 1,
+                               preL + leftSize,
+                               inL,
+                               k - 1);
         }
 
-        if (k < inR) {
-            bool foundRight = false;
-            int rightVal = 0;
-            int rightIndex = -1;
+        int rightIndex = preL + 1 + leftSize;
 
-            for (int x = preL + 1; x <= preR && !foundRight; x++) {
-                for (int j = k + 1; j <= inR; j++) {
-                    if (pre[x] == in[j]) {
-                        rightVal = pre[x];
-                        rightIndex = x;
-                        foundRight = true;
-                        break;
-                    }
-                }
-            }
-
-            if (foundRight) {
-                int rightSize = inR - k;
-                root->right = build(pre, in,
-                                    rightIndex, preR,
-                                    k + 1, inR);
-            }
+        if (rightIndex <= preR) {
+            root->right = build(pre, in,
+                                rightIndex,
+                                preR,
+                                k + 1,
+                                inR);
         }
 
         return root;
