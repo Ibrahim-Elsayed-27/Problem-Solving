6f7c6fb2109977f35bfdfb9aebb3c7b36a8d1e8f
Ibrahim-Elsayed-27 <95621621+Ibrahim-Elsayed-27@users.noreply.github.com>
2025-12-22 12:27:14 +0200

    1668-maximum-repeating-substring .cpp Easy 12-22-2025 2 ms (11.7%) 9.2 MB (14.8%)

--- LeetCode/Easy/1668-maximum-repeating-substring/1668-maximum-repeating-substring.cpp
+++ LeetCode/Easy/1668-maximum-repeating-substring/1668-maximum-repeating-substring.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
     int maxRepeating(string sequence, string word) {
+        vector<int> seq_dp(sequence.size(), -1);
         int max_count = 0;
         int count = 0;
-        for(int k=0; k<sequence.size();++k){
-            for(int i=k; i<sequence.size(); ++i){
+        for (int k = 0; k < sequence.size(); ++k) {
+            for (int i = k; i < sequence.size(); ++i) {
                 bool flag = true;
-                for(int j=0; j<word.size(); ++j){
-                    if(sequence[i+j] != word[j]){
-                        count = 0;
-                        flag = false;
-                        break;
+                if (seq_dp[i] == -1) {
+                    for (int j = 0; j < word.size(); ++j) {
+                        if (sequence[i + j] != word[j]) {
+                            count = 0;
+                            seq_dp[i] = 0;
+                            flag = false;
+                            break;
+                        }
                     }
-                }
-                if(flag){
-                    ++count;
-                    max_count = (max_count< count? count: max_count);
-                    i+= word.size()-1;
-                }
 
+                    if (flag) {
+                        ++count;
+                        max_count = (max_count < count ? count : max_count);
+                        seq_dp[i] = 1;
+                        i += word.size() - 1;
+                    }
+                } else if (seq_dp[i] == 0) {
+                    i += word.size() - 1;
+                    count = 0;
+                } else {
+                    ++count;
+                    max_count = (max_count < count ? count : max_count);
+                    i += word.size() - 1;
+                }
             }
 
             count = 0;
@@ -26,5 +38,4 @@ public:
 
         return max_count;
     }
-
 };
