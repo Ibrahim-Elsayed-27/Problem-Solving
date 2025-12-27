179674bbfd0477271b717500224fa6bf110fd9aa
Ibrahim-Elsayed-27 <95621621+Ibrahim-Elsayed-27@users.noreply.github.com>
2025-12-27 16:10:24 +0200

    0022-generate-parentheses .cpp Medium 12-27-2025 3 ms (71.5%) 16.5 MB (12.5%)

--- LeetCode/Medium/0022-generate-parentheses/0022-generate-parentheses.cpp
+++ LeetCode/Medium/0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -2,23 +2,31 @@ class Solution {
 public:
     vector<string> generateParenthesis(int n) {
         vector<string> result;
-
-        function<void(string, int, int)> backtrack = [&](string current, int open, int close) {
-            if (open == 0 && close == 0) {
-                result.push_back(current);
-                return;
-            }
-
-            if (open > 0) {
-                backtrack(current + '(', open - 1, close);
-            }
-
-            if (close > open) {
-                backtrack(current + ')', open, close - 1);
-            }
-        };
-
-        backtrack("", n, n);
+        string combination;
+        generateParenthesisDFS(result, combination, n, n);
         return result;
+    }
+
+    void generateParenthesisDFS(vector<string>& result, string combination, int open_count, int close_count){
+        if(!open_count && !close_count){
+             result.push_back(combination);
+             return;
+        }
+        if(open_count){
+            char open = '(';
+            string temp1 = combination;
+            temp1+=open;
+            generateParenthesisDFS(result, temp1, open_count-1, close_count);
+        }
+        
+        if(close_count > open_count){
+            char close = ')';
+            string temp2 = combination;
+            temp2+= close;
+            generateParenthesisDFS(result, temp2, open_count, close_count-1);
+        }
+
+        return;
+
     }
 };
