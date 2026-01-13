ad41df6e6212e96e109171b40d80e05e5cc52582
Ibrahim-Elsayed-27 <95621621+Ibrahim-Elsayed-27@users.noreply.github.com>
2026-01-13 11:33:54 +0200

    0904-fruit-into-baskets .cpp Medium 01-13-2026 N/A (0%) N/A (0%)

--- LeetCode/Medium/0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ LeetCode/Medium/0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -11,39 +11,37 @@ public:
         two_fruits[0][0] = fruits[0]; two_fruits[0][1] = 0;
         int fruit_count = 1; 
 
-        for (int end_window = 0; end_window < n; ++end_window) {
-            int curr = fruits[end_window];
+        for(int end_window = 1; end_window < n; ++end_window){
 
             bool found = false;
-            for (int i = 0; i < fruit_count; ++i) {
-                if (two_fruits[i][0] == curr) {
-                    two_fruits[i][1] = end_window; 
+
+            for(int i=0; i< fruit_count; ++i){
+                if(two_fruits[i][0] == fruits[end_window]){
                     found = true;
-                    break;
+                    two_fruits[i][1] = end_window;
+                }else{
+                    if(fruit_count ==1){
+                        two_fruits[1][0] = fruits[end_window];
+                        two_fruits[1][1] = end_window;
+                        found = true;
+                        fruit_count++;
+                    }
                 }
             }
 
-            if (!found) {
-                if (fruit_count < 2) {
-                    two_fruits[fruit_count][0] = curr;
-                    two_fruits[fruit_count][1] = end_window;
-                    fruit_count++;
-                } else {
-                    int remove_idx = (two_fruits[0][1] < two_fruits[1][1]) ? 0 : 1;
-                    int keep_idx = 1 - remove_idx;
-
-                    start_window = two_fruits[remove_idx][1] + 1;
-
-                    
-                    two_fruits[remove_idx][0] = curr;
-                    two_fruits[remove_idx][1] = end_window;
+            if(!found){
+                if(two_fruits[0][1] < two_fruits[1][1]){
+                    start_window = two_fruits[0][1] + 1;
+                    two_fruits[0][0] = fruits[end_window];
+                }else{
+                    start_window = two_fruits[1][1] + 1;
+                    two_fruits[1][0] = fruits[end_window];                    
                 }
             }
 
-            
-            max_fruits = max(max_fruits, end_window - start_window + 1);
+            max_fruits = max(max_fruits, end_window - start_window);
         }
 
-        return max_fruits;
+        return max_fruits+1;
     }
 };
