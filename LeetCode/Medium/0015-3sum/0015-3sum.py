201190586bad7d23e12c746a1ddb7c5c3a5d8b52
Ibrahim-Elsayed-27 <95621621+Ibrahim-Elsayed-27@users.noreply.github.com>
2026-02-11 17:57:58 +0200

    0015-3sum .py Medium 02-11-2026 2524 ms (5%) 24.5 MB (5.45%)

---
+++ LeetCode/Medium/0015-3sum/0015-3sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        triplets = set()
+        nums.sort()
+        #print(nums)
+        dict_nums = {val: i for i, val in enumerate(nums)}
+        for i in range(len(nums) - 2):
+            for j in range(i+1, len(nums)):
+                temp = -nums[i] - nums[j]
+                temp_index = dict_nums.get(temp)
+                if temp_index != None:
+                    if(temp_index > j):
+                        triple = tuple((nums[i], nums[j], temp))
+                        triplets.add(triple)
+
+        return [list(t) for t in triplets]
