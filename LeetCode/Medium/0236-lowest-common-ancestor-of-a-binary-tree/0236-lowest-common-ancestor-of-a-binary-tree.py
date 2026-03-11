3abf235809ff8dc1aadb4b51b8ef826c156153b1
Ibrahim Elsayed <himahema858@gmail.com>
2026-03-12 01:39:45 +0200

    0236-lowest-common-ancestor-of-a-binary-tree .py Medium 03-12-2026 65 ms (29.97%) 24.7 MB (27.01%)

---
+++ LeetCode/Medium/0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,50 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+def depth_of_node(root, node, depth=0, path=None):
+    if path is None:
+        path = []
+
+    if root is None:
+        return None
+
+    path.append(root)
+
+    if root == node:
+        return depth, path.copy()
+
+    left = depth_of_node(root.left, node, depth + 1, path)
+    if left is not None:
+        return left
+
+    right = depth_of_node(root.right, node, depth + 1, path)
+    if right is not None:
+        return right
+
+    path.pop()
+    return None
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+
+        p_depth, p_path = depth_of_node(root, p)
+        q_depth, q_path = depth_of_node(root, q)
+        i = 0
+        while(i < len(p_path) and i < len(q_path)):
+            if p_path[i] != q_path[i]:
+                return p_path[i - 1]
+            
+            i += 1
+
+        if(i == len(p_path)):
+            return p_path[-1]
+        else:
+            return q_path[-1]
+        
+        
+        
+
+
+
