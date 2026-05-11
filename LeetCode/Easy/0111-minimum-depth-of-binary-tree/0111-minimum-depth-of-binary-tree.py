c655861ffdec06c085f9854b4735487647083e05
Ibrahim Elsayed <himahema858@gmail.com>
2026-05-11 22:12:50 +0300

    0111-minimum-depth-of-binary-tree .py Easy 05-11-2026 44 ms (34.56%) 45.1 MB (28.26%)

--- LeetCode/Easy/0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
+++ LeetCode/Easy/0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
@@ -4,17 +4,17 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-def dfs(root):
-    if(not root):
-        return 0
 
-    left = dfs(root.left)
-    right = dfs(root.right)
-    if(left == 0):
-        return right + 1
-    if(right == 0):
-        return left + 1
-    return min(left, right) + 1
+
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
-        return dfs(root)
+        if(not root):
+            return 0
+
+        left = self.minDepth(root.left)
+        right = self.minDepth(root.right)
+        if(left == 0):
+            return right + 1
+        if(right == 0):
+            return left + 1
+        return min(left, right) + 1
